cfec0638cf80ee08f59b888b4453bc0640f5db19
Stephen Tippets <stephen.tippets@gmail.com>
2016-08-04 12:46:39 -0600

    Add additional template views for styline

--- cattle/urls.py
+++ cattle/urls.py
@@ -3,4 +3,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+    url(r'^about', TemplateView.as_view(template_name="about.html"), name='index'),
+    url(r'^offering', TemplateView.as_view(template_name="offering.html"), name='index'),
+    url(r'^newsletter', TemplateView.as_view(template_name="newsletter.html"), name='index'),
+    url(r'^contact', TemplateView.as_view(template_name="contact.html"), name='index'),
 ]
