f8f59da2fb5537112f9e1744eee81f7ae899fb28
Stephen Tippets <stephen.tippets@gmail.com>
2016-08-04 14:20:49 -0600

    Make homepage mostly responsive

---
+++ cattle/static/cattle/js/main.js
@@ -0,0 +1,8 @@
+;(function ($){
+  $(function() {
+    //Main menu mobile button click
+    $('.mobile-menu-button').click(function() {
+      $('.main-menu').toggleClass('open');
+    });
+  });
+})(jQuery);
