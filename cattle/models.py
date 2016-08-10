db65ccf5fc6f38759d52a1edd467d1a89866df5d
Mike Gallagher <gallaghermikey@gmail.com>
2016-08-09 21:59:46 -0600

    Initial cattle model

--- cattle/models.py
+++ cattle/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Cattle(models.Model):
+    """
+    Cattle model
+    """
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDERS = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+
+    producer = models.ForeignKey(User)
+    zip = models.CharField(max_length=5)
+    lot_num = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=50)
+    sex = models.CharField(max_length=1, choices=GENDERS)
