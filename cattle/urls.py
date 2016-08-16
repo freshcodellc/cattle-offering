c6f72c33a6dfee68a40973d1d6c5e745e637b481
Stephen Tippets <stephen.tippets@gmail.com>
2016-08-16 00:06:13 -0600

    Add all fields for cattle to offering view and detail view

--- cattle/urls.py
+++ cattle/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
-from .views import CattleListView
+from .views import CattleListView, CattleDetailView
 
 urlpatterns = [
+    url(r'^offering/(?P<pk>[-\w]+)/$', CattleDetailView.as_view(), name='offering-detail'),
     url(r'^offering', CattleListView.as_view(), name='offering'),
 ]
--- cattle/views.py
+++ cattle/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Cattle
@@ -6,3 +6,7 @@ from .models import Cattle
 class CattleListView(ListView):
     model = Cattle
     template_name = 'cattle/offering.html'
+
+class CattleDetailView(DetailView):
+    model = Cattle
+    template_name = 'cattle/offering_detail.html'
