c2c8acb740ce4cc87168f37dc5073ea8b9e8061c
Derek Tippets <derektippets@Dereks-MacBook-Air.local>
2016-08-24 15:10:47 -0600

    Added contact form functionality.

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 
 
 class ContactForm(forms.Form):
@@ -8,5 +10,21 @@ class ContactForm(forms.Form):
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'contact-form__message'}))
 
     def send_email(self):
-        # TODO: send email using the self.cleaned_data dictionary
-        pass
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        subject = self.cleaned_data['subject']
+        message = self.cleaned_data['message']
+        from_email = 'contact@cattleoffering.com'
+        context = {'name': name, 'email': email,'subject': subject, 'message': message}
+
+        text_message = render_to_string('emails/contact.txt', context)
+        html_message = render_to_string('emails/contact.html', context)
+
+        if subject and message and email:
+            try:
+                send_mail(subject, text_message, from_email, ['cody.montgomery@gmail.com'], False,
+                          None, None, None, html_message)
+            except:
+                return False
+        else:
+            return False
